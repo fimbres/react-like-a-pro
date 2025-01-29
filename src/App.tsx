41bc548796619e1383e72c6f4dcbc687755ddd29
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-01-28 23:34:23 -0800

    taking react to the next level

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" width={100} />
+      </a>
+      <h1>App.tsx</h1>
+    </>
+  )
+}
+
+export default App
