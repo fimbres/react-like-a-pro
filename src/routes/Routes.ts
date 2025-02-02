b387eea33d4b11c43973b643d202e0a25bb349f4
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-01 23:48:39 -0800

    creating new routes

---
+++ src/routes/Routes.ts
@@ -0,0 +1,31 @@
+import { lazy, LazyExoticComponent, type ReactElement } from "react";
+
+const LazyPage = lazy(() => import(/* webpackChunckName: "lazypage" */ '../componens/lazy-loading/pages/LazyPage'));
+const LazyPage2 = lazy(() => import(/* webpackChunckName: "lazypage-2" */ '../componens/lazy-loading/pages/LazyPage2'));
+const LazyPage3 = lazy(() => import(/* webpackChunckName: "lazypage-3" */ '../componens/lazy-loading/pages/LazyPage3'));
+
+type Page = () => ReactElement;
+
+export interface Route {
+  path: string;
+  label: string;
+  Component: LazyExoticComponent<Page> | Page;
+}
+
+export const routes: Route[] = [
+  {
+    path: '/lazy-1',
+    label: 'Lazy Page',
+    Component: LazyPage,
+  },
+  {
+    path: '/lazy-2',
+    label: 'Lazy Page 2',
+    Component: LazyPage2,
+  },
+  {
+    path: '/lazy-3',
+    label: 'Lazy Page 3',
+    Component: LazyPage3,
+  },
+];
