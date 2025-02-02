bf5ec50baa75f48cf5df69850f443fb8daec54f1
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-01 23:47:56 -0800

    new dummy components

---
+++ src/componens/lazy-loading/pages/LazyPage.tsx
@@ -0,0 +1,9 @@
+const LazyPage = () => {
+  return (
+    <div>
+      <h1>Lazy Page 1</h1>
+    </div>
+  )
+}
+
+export default LazyPage;
---
+++ src/componens/lazy-loading/pages/LazyPage3.tsx
@@ -0,0 +1,9 @@
+const LazyPage3 = () => {
+  return (
+    <div>
+      <h1>Lazy Page 3</h1>
+    </div>
+  )
+}
+
+export default LazyPage3;
