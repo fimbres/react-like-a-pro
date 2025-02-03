bc02a647934607ddd598d9c5e6cdcb089e5b63f8
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-02 16:15:50 -0800

    extracting products data

---
+++ src/data/products.ts
@@ -0,0 +1,19 @@
+import { Product } from "../interfaces/ProductCard";
+
+export const products: Product[] = [
+  {
+    id: "1",
+    image: "./car.webp",
+    title: "Tundra 2025"
+  },
+  {
+    id: "2",
+    image: "./car2.png",
+    title: "CRV 2015"
+  },
+  {
+    id: "3",
+    image: "./car3.png",
+    title: "C200 2017"
+  },
+];
