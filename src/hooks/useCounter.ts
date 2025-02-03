d9458cb29b594481a1813e227c50560ff84089cb
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-02 16:16:03 -0800

    adding logic in useCounter hook

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export const useCounter = (defaultVal: number = 0, minVal: number = 0) => {
-  const [counter, setCounter] = useState(defaultVal);
+interface useCounterProps {
+  value: number;
+  defaultVal?: number;
+  minVal?: number;
+  onChange?: (quantity: number) => void;
+}
+
+export const useCounter = ({ minVal = 0, value, onChange } : useCounterProps) => {
+  const [counter, setCounter] = useState(value);
+  const isControlled = useRef(onChange);
 
   const increaseBy = (value: number) => {
-    setCounter(v => v + value >= minVal ? v + value : v);
+    const newValue = counter + value;
+
+    if(newValue >= minVal && isControlled.current) {
+      setCounter(newValue);
+      onChange?.(newValue);
+    }
   };
+
+  useEffect(() => {
+    setCounter(value);
+  }, [value]);
 
   return {
     counter,
